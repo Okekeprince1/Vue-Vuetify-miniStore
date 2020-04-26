7625ee6b36d0b47c43f73f1d20f6064cb4e155bd
Okekeprince1 <okeke.p.ebube@gmail.com>
2020-04-26 21:54:44 +0100

    Added card but without the complete images

---
+++ src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav>
+    <v-app-bar color="white" dark app class="line">
+      <v-toolbar-title class="text-uppercase red--text">
+        <span class="font-weight-light">Cloud</span>
+        <span>Industry</span>
+      </v-toolbar-title>
+      <template>
+        <v-btn color="red" small dark absolute right bottom fab>
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </template>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" fixed dark app mini-variant class="red">
+      <v-list flat>
+        <v-list-item class="mb-12">
+          <v-list-item-action>
+            <v-icon>fas fa-cloud-download-alt</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+
+        <v-list-item router to="/">
+          <v-list-item-action>
+            <v-icon>fas fa-desktop</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+
+        <v-list-item router to="/">
+          <v-list-item-action>
+            <v-badge color="yellow">
+              <template v-slot:badge>1</template>
+              <v-icon>fas fa-shopping-cart</v-icon>
+            </v-badge>
+          </v-list-item-action>
+        </v-list-item>
+
+        <v-list-item router to="/">
+          <v-list-item-action>
+            <v-icon>fas fa-city</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+
+        <v-list-item router to="/">
+          <v-list-item-action>
+            <v-icon>fas fa-cog</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: true
+  })
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,154 @@
+<template>
+  <v-app :style="{background: $vuetify.theme.themes.light.background}">
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field label="Search items" prepend-icon="search" color="red" error></v-text-field>
+        </v-col>
+        <v-spacer></v-spacer>
+        <span class="red--text mt-9">29 items</span>
+        <v-btn text class="mt-7" color="red">
+          <v-icon color="red" class="mr-n10">menu</v-icon>
+        </v-btn>
+        <v-btn text class="mt-7" color="red">
+          <v-icon color="red" class="mr-n10">dashboard</v-icon>
+        </v-btn>
+      </v-row>
+
+      <v-row>
+        <v-col cols="12" md="3" class="md-4">
+          <v-hover v-slot:default="{hover}">
+            <v-card max-hieght="250" max-width="250" align="center">
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon>edit</v-icon>
+                </v-btn>
+              </v-card-actions>
+              <v-img src="01.png" width="200" height="200"></v-img>
+              <v-card-text class="red--text">Air Max Hyper Agressor</v-card-text>
+              <v-expand-transition>
+                <div
+                  v-if="hover"
+                  class="d-flex transition-fast-in-fast-out red v-card--reveal display-3 white--text"
+                  style="height: 100%"
+                >
+                  <v-btn outlined color="white">Details</v-btn>
+                </div>
+              </v-expand-transition>
+              <v-btn absolute="" color="red" class="white--text" large fab top left>
+              <h1>1</h1>
+              </v-btn>
+            </v-card>
+          </v-hover>
+        </v-col>
+
+         <v-col cols="12" md="3" class="md-4">
+          <v-hover v-slot:default="{hover}">
+            <v-card max-hieght="250" max-width="250" align="center">
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon>edit</v-icon>
+                </v-btn>
+              </v-card-actions>
+              <v-img src="01.png" width="200" height="200"></v-img>
+              <v-card-text class="red--text">Air Max Hyper Agressor</v-card-text>
+              <v-expand-transition>
+                <div
+                  v-if="hover"
+                  class="d-flex transition-fast-in-fast-out red v-card--reveal display-3 white--text"
+                  style="height: 100%"
+                >
+                  <v-btn outlined color="white">Details</v-btn>
+                </div>
+              </v-expand-transition>
+              <v-btn absolute="" color="red" class="white--text" large fab top left>
+              <h1>1</h1>
+              </v-btn>
+            </v-card>
+          </v-hover>
+        </v-col>
+
+         <v-col cols="12" md="3" class="md-4">
+          <v-hover v-slot:default="{hover}">
+            <v-card max-hieght="250" max-width="250" align="center">
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon>edit</v-icon>
+                </v-btn>
+              </v-card-actions>
+              <v-img src="01.png" width="200" height="200"></v-img>
+              <v-card-text class="red--text">Air Max Hyper Agressor</v-card-text>
+              <v-expand-transition>
+                <div
+                  v-if="hover"
+                  class="d-flex transition-fast-in-fast-out red v-card--reveal display-3 white--text"
+                  style="height: 100%"
+                >
+                  <v-btn outlined color="white">Details</v-btn>
+                </div>
+              </v-expand-transition>
+              <v-btn absolute="" color="red" class="white--text" large fab top left>
+              <h1>1</h1>
+              </v-btn>
+            </v-card>
+          </v-hover>
+        </v-col>
+
+         <v-col cols="12" md="3" class="md-4">
+          <v-hover v-slot:default="{hover}">
+            <v-card max-hieght="250" max-width="250" align="center">
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon>edit</v-icon>
+                </v-btn>
+              </v-card-actions>
+              <v-img src="01.png" width="200" height="200"></v-img>
+              <v-card-text class="red--text">Air Max Hyper Agressor</v-card-text>
+              <v-expand-transition>
+                <div
+                  v-if="hover"
+                  class="d-flex transition-fast-in-fast-out red v-card--reveal display-3 white--text"
+                  style="height: 100%"
+                >
+                  <v-btn outlined color="white">Details</v-btn>
+                </div>
+              </v-expand-transition>
+              <v-btn absolute="" color="red" class="white--text" large fab top left>
+              <h1>1</h1>
+              </v-btn>
+            </v-card>
+          </v-hover>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+  computed: {
+    theme() {
+      return this.$vuetify.theme.dark ? "dark" : "light";
+    }
+  }
+};
+</script>
+
+<style>
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: 0.5;
+  position: absolute;
+  width: 100%;
+}
+</style>
